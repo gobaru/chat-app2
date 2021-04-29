510f2ad86c661bf00b7ff45f2c5fca1e6e16d006
gobaru <gobaru.1993@gmail.com>
2021-04-29 12:33:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  with_options presence: true do
+    validates :name
+    validates :email
+  end
+
 end
